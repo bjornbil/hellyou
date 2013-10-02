d8bca97f6776913ca407a825e1f14ee2c3474a9d
bjornbil <bjorn.billen@student.khleuven.be>
2013-10-02 11:51:15 +0200

    Revert "first commit helloyou, native + webapp"
    This reverts commit 8d51c38b7ffe582f30e8492e73b331475449bf5b.

--- WebApp/public_html/js/functions.js
+++
@@ -1,9 +0,0 @@
- function hello() {
-                var input = document.getElementById("name").value;
-                if (input.length === 0) {
-                    alert("Please enter a valid name!");
-                } else {
-                    document.getElementById("output").textContent = "Hello " + input + ", this is a web app!";
-                }
-                return false;
-            }
